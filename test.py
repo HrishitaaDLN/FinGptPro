202b67900120aea0651df618fbb828b6db3997ce
HrishitaaDLN <ldhar@uic.edu>
2025-10-29 00:28:12 -0500

    Remove deletion of 'finrag_docs' collection
    Removed code for deleting a collection in Qdrant.

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-from qdrant_client import QdrantClient
 
-client = QdrantClient(host="localhost", port=6333)
-client.delete_collection("finrag_docs")
-print("🗑️ Deleted old collection: finrag_docs")
